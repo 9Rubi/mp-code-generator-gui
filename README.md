167ac189f42cdbb8ad87d65aba76af8a0050f60e
9Rubi <1344760467@qq.com>
2019-05-18 18:55:01 +0800

    .

---
+++ README.md
@@ -0,0 +1,11 @@
+##这是什么jb?
+
+Mybatis-plus 的 代码生成器 的 GUI版
+
+pom依赖里的 velocity 和 spring 是为了兼容
+
+##能干什么?
+
+生成代码,do the same as the **mybatis-plus-generator**
+
+还有部分功能未实现
--- src/main/java/ink/rubi/App.java
+++ src/main/java/ink/rubi/App.java
@@ -49,43 +49,12 @@ public class App extends Application {
         prop.load(getClass().getResourceAsStream("/conf.properties"));
         prop.forEach((key, value) -> log.info(key + ":" + value));
 
-
         log.info("加载样式");
         scene = new Scene(layout);
         scene.getStylesheets().add("/pattern/App.css");
 
-        /*new Thread(() -> {
-            for (; ; ) {
-                try {
-                    TimeUnit.SECONDS.sleep(2);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                log.info("这一是一条日志,this is a log,time is {}", LocalDateTime.now());
-                TextArea textArea = (TextArea)layout.lookup("#console");
-                log.info("{}",textArea);
-            }
-        }).start();*/
         window.setScene(scene);
         window.show();
     }
 
-
-    public void getStream() {
-
-        ByteArrayOutputStream baoStream = new ByteArrayOutputStream(1024);
-        PrintStream cacheStream = new PrintStream(baoStream);
-        //创建还原给控制台输出的输出流
-        PrintStream oldStream = System.out;
-        //截取控制台输出信息
-        System.setOut(cacheStream);
-        //这里开始的输出全部可以截取到
-        System.out.println(111);
-        //将截取的信息转换成字符串
-        String message = baoStream.toString();
-        //截取完毕，将输出信息返回给控制台
-        System.setOut(oldStream);
-
-        System.out.println(message);
-    }
 }
