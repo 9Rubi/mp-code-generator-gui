0e79baecd69c18a3595e72c63b7936da24afe617
e <1344760467@qq.com>
2019-06-18 10:36:04 +0800

    .

--- src/main/java/ink/rubi/coffee/App.java
+++ src/main/java/ink/rubi/coffee/App.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import lombok.Getter;
 import org.slf4j.Logger;
@@ -36,12 +37,13 @@ public class App extends Application {
     public void start(Stage primaryStage) throws Exception {
         window = primaryStage;
 
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/pattern/App.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/asset/fxml/App.fxml"));
         layout = fxmlLoader.load();
 
         window.setResizable(false);
-//        window.setMax
+
         window.setTitle("Mybatis-plus代码生成器GUI");
+        window.getIcons().add(new Image(getClass().getResourceAsStream("/asset/img/coffee.png")));
 
         scene = new Scene(layout);
         window.setScene(scene);
